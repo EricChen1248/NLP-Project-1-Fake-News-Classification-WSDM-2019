6ead1e0d3ce6d653e8a8c4d12cc7d384ae20b1b8
Eric Chen <ericchen1248@gmail.com>
2019-04-23 16:30:53 +0800

    Word Embedding Model for discerning related/nonrelated

---
+++ training.py
@@ -0,0 +1,19 @@
+import csv
+from gensim.models import Word2Vec
+
+print("====== Training Word2Vec =======")
+
+count = 0
+
+titles = []
+with open('cutTrainingData.csv', 'r') as csvFile:
+    reader = csv.DictReader(csvFile, delimiter=',')
+    for row in reader:
+        titles.append(row['title1'].split('/'))
+        titles.append(row['title2'].split('/'))
+        
+        if count >= 290_000:
+            break
+
+model = Word2Vec(titles, size=80, window=2, min_count=10, workers=16)
+model.save("word2vec_model")
