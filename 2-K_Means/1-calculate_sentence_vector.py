2149a0c525346d0130e524721d0fecc30b5f6bce
FTS152 <b04705007@ntu.edu.tw>
2019-05-10 23:47:15 +0800

    kmeans modify(add testing feature generation)

--- 2-K_Means/1-calculate_sentence_vector.py
+++ 2-K_Means/1-calculate_sentence_vector.py
@@ -4,13 +4,11 @@ import numpy as np
 from gensim.models import Word2Vec
 
 
-#%%
 model = Word2Vec.load("./data/word2vec_model")
 
 dataFile = "./data/cut_training.csv"
 data = pd.read_csv(dataFile, delimiter=',')
 
-#%%
 ids = [['id1', 'title1'], ['id2', 'title2']]
 vectors = {}
 c = 0
@@ -27,6 +25,29 @@ for _, row in data.iterrows():
     print(f"Calculating Score: ({'#' * int(pCount)}{'-' * (100 - int(pCount))}) {pCount:.2f}%", end='\r') 
 
 print(' ' * 140)
-#%%
+
 np.save('./data/sentence_vectors.npy', vectors)
+
+
+dataFile = "./data/cut_testing.csv"
+data = pd.read_csv(dataFile, delimiter=',')
+
+ids = [['id1', 'title1'], ['id2', 'title2']]
+vectors = {}
+c = 0
+for _, row in data.iterrows():
+    for id, title in ids:
+        if row[id] not in vectors:
+            try:
+                vectors[row[id]] = np.mean([model.wv[w] if w in model.wv else 0 for w in row[title].split('/')], axis=0).tolist()
+            except:
+                pass
+        
+    c += 1
+    pCount = c * 100 / data.shape[0]
+    print(f"Calculating Score: ({'#' * int(pCount)}{'-' * (100 - int(pCount))}) {pCount:.2f}%", end='\r') 
+
+print(' ' * 140)
+
+np.save('./data/sentence_vectors_testing.npy', vectors)
     
--- 2-K_Means/2-k_means_on_sentence_vectors.py
+++ 2-K_Means/2-k_means_on_sentence_vectors.py
@@ -1,14 +1,11 @@
-#%%
 import pandas as pd
 import numpy as np
 import json
 
-#%%
-dataFile = "./data/cut_training.csv"
+dataFile = "./data/cut_testing.csv"
 data = pd.read_csv(dataFile, delimiter=',')
 
-vectors = np.load('./data/sentence_vectors.npy').item() 
-#%%
+vectors = np.load('./data/sentence_vectors_testing.npy').item() 
 class group:
     def __init__(self, vect, id):
         self.vector = np.array(vect)
@@ -23,11 +20,77 @@ class group:
 def similarity(g1, g2):
     return np.dot(g1.vector, g2.vector) /np.linalg.norm(g1.vector) / np.linalg.norm(g2.vector)
 
-#%%
+vectors = {int(k):group(v, int(k)) for k,v in vectors.items()}
+
+changed = True
+i = 0
+rows = range(len(data))
+while changed:
+    i += 1
+    changed = False
+    c = 0
+    
+    newRow = []
+    length = len(rows)
+    print(f"Iteration {i} with {length} rows")
+    for r in rows:
+        row = data.iloc[r]
+        try:
+            if similarity(vectors[row['id1']], vectors[row['id2']]) > 0.9 :
+                vectors[row['id1']].combine(vectors[row['id2']])
+                vectors[row['id2']] = vectors[row['id1']]
+                changed = True
+            else:
+                newRow.append(r)
+        except:
+            pass
+        
+        c += 1
+        pCount = c * 100 / len(rows)
+        print(f"Finding Neighbours: ({'#' * int(pCount)}{'-' * (100 - int(pCount))}) {pCount:.2f}%", end='\r') 
+    
+    rows = newRow
+    print(' ' * 140, end='\r')
+
+score = np.zeros(data.shape[0])
+c = 0
+for _, row in data.iterrows():
+    try:
+        score[c] = similarity(vectors[row['id1']],vectors[row['id2']])
+    except:
+        pass
+
+    
+    c += 1
+    pCount = c * 100 / data.shape[0]
+    print(f"Calculating Scores: ({'#' * int(pCount)}{'-' * (100 - int(pCount))}) {pCount:.2f}%", end='\r') 
+
+print(' ' * 140)
+np.save('./data/k-means-score-test.npy', score)
+
+
+
+
+dataFile = "./data/cut_training.csv"
+data = pd.read_csv(dataFile, delimiter=',')
+
+vectors = np.load('./data/sentence_vectors.npy').item() 
+class group:
+    def __init__(self, vect, id):
+        self.vector = np.array(vect)
+        self.ids = [id]
+
+    def combine(self, other):
+        self.vector = self.vector + other.vector
+        self.ids += other.ids
+        other.ids = None
+        other.vector = None
+
+def similarity(g1, g2):
+    return np.dot(g1.vector, g2.vector) /np.linalg.norm(g1.vector) / np.linalg.norm(g2.vector)
 
 vectors = {int(k):group(v, int(k)) for k,v in vectors.items()}
 
-#%%
 changed = True
 i = 0
 rows = range(len(data))
